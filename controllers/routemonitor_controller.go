edd9dafab1981a43be477b2f8bae88f0c2de23c7
Ron Green <11993626+georgettica@users.noreply.github.com>
2020-09-08 15:06:18 +0300

    fix(rotemonitor): make routemonitor create servicemonitor
    - added a configmap which is not required
    - made reconcilerestart when error
    -

--- controllers/routemonitor_controller.go
+++ controllers/routemonitor_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
@@ -26,6 +27,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+
 	//"k8s.io/apimachinery/pkg/util/intstr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -49,6 +51,7 @@ type RouteMonitorReconciler struct {
 func (r *RouteMonitorReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
 	log := r.Log.WithValues("routeMonitor", req.NamespacedName)
+	namespacedName := types.NamespacedName{Name: "blackbox-exporter", Namespace: "openshift-monitoring"}
 
 	// Fetch all routeMonitors
 	// It doesn't matter how many there are, we only ever deploy one deploymentForBlackboxExporter
@@ -57,27 +60,50 @@ func (r *RouteMonitorReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error
 	if err != nil {
 		log.Error(err, "Failed to get routeMonitor")
 	}
+
+	// Check if there's a blackbox_exporter configmap
+	// If not create one
+	foundBlackboxConfigMap := &corev1.ConfigMap{}
+	// To make sure we only create one, we hardcode the name instead of using routeMonitor names
+	err = r.Get(ctx, namespacedName, foundBlackboxConfigMap)
+	if err != nil {
+		// Is it not found?
+		if errors.IsNotFound(err) {
+			// Create the configmap
+			boxConf := r.configmapForBlackboxExporter(namespacedName)
+			log.Info("Creating a new Configmap", "ConfigMap.Namespace", boxConf.Namespace, "ConfigMap.Name", boxConf.Name)
+			err = r.Create(ctx, boxConf)
+			if err != nil {
+				log.Error(err, "Failed to create new ConfigMap", "ConfigMap.Namespace", boxConf.Namespace, "ConfigMap.Name", boxConf.Name)
+				return ctrl.Result{}, err
+			}
+			// Seems to be all good, should requeue for next step
+			return ctrl.Result{RequeueAfter: 5 * time.Second}, nil
+		}
+	}
+
 	// Check if there's a blackbox_exporter deployment
 	// If not create one
 	foundBlackboxDeployment := &appsv1.Deployment{}
 	// To make sure we only create one, we hardcode the name instead of using routeMonitor names
-	err = r.Get(ctx, types.NamespacedName{Name: "blackbox-exporter", Namespace: "openshift-monitoring"}, foundBlackboxDeployment)
+	err = r.Get(ctx, namespacedName, foundBlackboxDeployment)
 	if err != nil {
 		// Is it not found?
 		if errors.IsNotFound(err) {
 			// Create the deployment
-			boxDep := r.deploymentForBlackboxExporter(routeMonitor)
+			boxDep := r.deploymentForBlackboxExporter(namespacedName)
 			log.Info("Creating a new Deployment", "Deployment.Namespace", boxDep.Namespace, "Deployment.Name", boxDep.Name)
 			err = r.Create(ctx, boxDep)
 			if err != nil {
 				log.Error(err, "Failed to create new Deployment", "Deployment.Namespace", boxDep.Namespace, "Deployment.Name", boxDep.Name)
 				return ctrl.Result{}, err
 			}
-			// Seems to be all good, if not return an err at least
-			return ctrl.Result{}, err
+			// Seems to be all good, should requeue for next step
+			return ctrl.Result{RequeueAfter: 5 * time.Second}, nil
 		}
 	}
 
+	log.Info("finished blackboxdeploy, starting servicemonitor")
 	// Check if a servicemonitor for each probe exists
 	// If not create them
 	foundServiceMonitor := &monitoringv1.ServiceMonitor{}
@@ -87,6 +113,7 @@ func (r *RouteMonitorReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error
 		// Is it not found?
 		if errors.IsNotFound(err) {
 			// Create the ServiceMonitor
+			log.Info("servicemonitor not found, creating")
 			serviceMonitorDep := r.deploymentForServiceMonitor(routeMonitor)
 			log.Info("Creating a new ServiceMonitor", "Deployment.Namespace", serviceMonitorDep.Namespace, "Deployment.Name", serviceMonitorDep.Name)
 			err = r.Create(ctx, serviceMonitorDep)
@@ -94,23 +121,24 @@ func (r *RouteMonitorReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error
 				log.Error(err, "Failed to create new ServiceMonitor", "Deployment.Namespace", serviceMonitorDep.Namespace, "Deployment.Name", serviceMonitorDep.Name)
 				return ctrl.Result{}, err
 			}
-			return ctrl.Result{}, err
+			return ctrl.Result{}, nil
 		}
+		log.Info("servicemonitor exists, nothing to do", foundServiceMonitor.ObjectMeta.Name, foundServiceMonitor.ObjectMeta.Namespace)
 	}
 	return ctrl.Result{}, nil
 }
 
 // deploymentForBlackBoxExporter returns a blackbox deployment
-func (r *RouteMonitorReconciler) deploymentForBlackboxExporter(m *monitoringv1alpha1.RouteMonitor) *appsv1.Deployment {
-	ls := labelsForRouteMonitor(m.Name)
+func (r *RouteMonitorReconciler) deploymentForBlackboxExporter(namespacedName types.NamespacedName) *appsv1.Deployment {
+	ls := labelsForRouteMonitor(namespacedName.Name)
 	// hardcode the replicasize for no
 	//replicas := m.Spec.Size
 	var replicas int32 = 1
 
 	dep := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      m.Name,
-			Namespace: m.Namespace,
+			Name:      namespacedName.Name,
+			Namespace: namespacedName.Namespace,
 		},
 		Spec: appsv1.DeploymentSpec{
 			Replicas: &replicas,
@@ -123,9 +151,8 @@ func (r *RouteMonitorReconciler) deploymentForBlackboxExporter(m *monitoringv1al
 				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{{
-						Image:   "prom/blackbox-exporter:master",
-						Name:    "blackbox-exporter",
-						Command: []string{"--config.file=/config/blackbox.yml"},
+						Image: "prom/blackbox-exporter:master",
+						Name:  "blackbox-exporter",
 						Ports: []corev1.ContainerPort{{
 							ContainerPort: 9115,
 							Name:          "blackbox",
@@ -136,8 +163,23 @@ func (r *RouteMonitorReconciler) deploymentForBlackboxExporter(m *monitoringv1al
 		},
 	}
 	// Set BlackboxExporter instance as the owner and controller
-	ctrl.SetControllerReference(m, dep, r.Scheme)
+	// ctrl.SetControllerReference(m, dep, r.Scheme)
 	return dep
+}
+
+const blackboxConfig = `modules:
+  http_2xx:
+    prober: http
+`
+
+func (r *RouteMonitorReconciler) configmapForBlackboxExporter(namespacedName types.NamespacedName) *corev1.ConfigMap {
+	return &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      namespacedName.Name,
+			Namespace: namespacedName.Namespace,
+		},
+		Data: map[string]string{"blackbox.yml": blackboxConfig},
+	}
 }
 
 // deploymentForServiceMonitor returns a ServiceMonitor
@@ -157,27 +199,6 @@ func (r *RouteMonitorReconciler) deploymentForServiceMonitor(m *monitoringv1alph
 		"Target": {m.Spec.Url},
 	}
 
-	endpoint := monitoringv1.Endpoint{
-		// TODO use variable from blackbox exporter deployment
-		Port: "blackbox",
-		// Probe every 30s
-		Interval: "30s",
-		// Timeout has to be smaller than probe interval
-		ScrapeTimeout: "15s",
-		Path:          "/probe",
-		Scheme:        "http",
-		// TargetPort:    intstr.FromInt(9115),
-		Params: params,
-		MetricRelabelConfigs: []*monitoringv1.RelabelConfig{
-			//&monitoringv1.RelabelConfig{
-			{
-				Replacement:  m.Spec.Url,
-				SourceLabels: []string{},
-				TargetLabel:  "RouteMonitorUrl",
-			},
-		},
-	}
-
 	dep := &monitoringv1.ServiceMonitor{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: m.Name,
@@ -189,9 +210,29 @@ func (r *RouteMonitorReconciler) deploymentForServiceMonitor(m *monitoringv1alph
 			APIVersion: monitoringv1.SchemeGroupVersion.String(),
 		},
 		Spec: monitoringv1.ServiceMonitorSpec{
-			JobLabel:  m.Name,
-			Endpoints: []monitoringv1.Endpoint{endpoint},
-			Selector:  *ls,
+			JobLabel: m.Name,
+			Endpoints: []monitoringv1.Endpoint{
+				{
+					// TODO use variable from blackbox exporter deployment
+					Port: "blackbox",
+					// Probe every 30s
+					Interval: "30s",
+					// Timeout has to be smaller than probe interval
+					ScrapeTimeout: "15s",
+					Path:          "/probe",
+					Scheme:        "http",
+					// TargetPort:    intstr.FromInt(9115),
+					Params: params,
+					MetricRelabelConfigs: []*monitoringv1.RelabelConfig{
+						//&monitoringv1.RelabelConfig{
+						{
+							Replacement:  m.Spec.Url,
+							SourceLabels: []string{},
+							TargetLabel:  "RouteMonitorUrl",
+						},
+					},
+				}},
+			Selector: *ls,
 			NamespaceSelector: monitoringv1.NamespaceSelector{
 				MatchNames: []string{m.Namespace},
 			},
@@ -212,12 +253,13 @@ func getPodNames(pods []corev1.Pod) []string {
 
 // Make this a function in case of hardcoding, in case we need more labels in the future
 func labelsForRouteMonitor(name string) map[string]string {
-	return map[string]string{"app": "RouteMonitoringOperator"}
+	return map[string]string{"app": "blackbox-exporter"}
 }
 
 func (r *RouteMonitorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&monitoringv1alpha1.RouteMonitor{}).
 		Owns(&appsv1.Deployment{}).
+		Owns(&corev1.ConfigMap{}).
 		Complete(r)
 }
