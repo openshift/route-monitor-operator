384f35a083994ef69c8ae7e1530ec314b8bfe7ba
Ron Green <11993626+georgettica@users.noreply.github.com>
2020-12-13 10:08:01 +0200

    feat(hack): remove unnecessary code
    The code now only needs:
    - another annotation of 'createdAt'
    - fix of the 'replaces' field
    - copying all of the bundle files to the output

--- hack/generate-operator-bundle.py
+++ hack/generate-operator-bundle.py
@@ -36,7 +36,8 @@ route_monitor_operator_image = sys.argv[5]
 full_version = "%s.%s-%s" % (VERSION_BASE, git_num_commits, git_hash)
 print("Generating CSV for version: %s" % full_version)
 
-with open('bundle/manifests/route-monitor-operator.clusterserviceversion.yaml', 'r') as stream:
+BUNDLE_BASE_PATH = 'bundle/manifests'
+with open(f"{BUNDLE_BASE_PATH}/route-monitor-operator.clusterserviceversion.yaml", 'r') as stream:
     csv = yaml.safe_load(stream)
 
 if not os.path.exists(outdir):
@@ -46,100 +47,31 @@ version_dir = os.path.join(outdir, full_version)
 if not os.path.exists(version_dir):
     os.mkdir(version_dir)
 
-# Initialize CRD array in CSV
-csv['spec']['customresourcedefinitions']['owned'] = []
-
-# Copy all CSV files over to the bundle output dir:
-kustomize_output = subprocess.run(
-    ["kustomize", "build", "config/crd"], stdout=subprocess.PIPE).stdout
-for crd in yaml.safe_load_all(kustomize_output):
-    # Update CSV template customresourcedefinitions key
-    csv['spec']['customresourcedefinitions']['owned'].append(
-        {
-            "name": crd["metadata"]["name"],
-            "description": crd["spec"]["names"]["kind"],
-            "displayName": crd["spec"]["names"]["kind"],
-            "kind": crd["spec"]["names"]["kind"],
-            "version": crd["spec"]["version"]
-        }
-    )
-
-# Copy all prometheus yaml files over to the bundle output dir:
-
-kustomize_yamls = subprocess.run(
-    ["kustomize", "build", "config/prometheus"], stdout=subprocess.PIPE)
-
-for extracted_yaml in yaml.safe_load_all(kustomize_yamls.stdout):
-    with tempfile.NamedTemporaryFile(mode="w+t",
-                                     prefix='prometheus_', suffix='.yaml',
-                                     dir=version_dir,
-                                     delete=False) as temp_file:
-        yaml.dump(extracted_yaml, temp_file, default_flow_style=False)
-
-csv['spec']['install']['spec']['clusterPermissions'] = []
-
-# Add route-monitor-operator role to the CSV:
-kustomize_yamls = subprocess.run(
-    ["kustomize", "build", "config/rbac"], stdout=subprocess.PIPE)
-only_cluster_role_yamls = [y for y in yaml.safe_load_all(
-    kustomize_yamls.stdout) if y["kind"] == "ClusterRole"]
-for operator_role in only_cluster_role_yamls:
-    csv['spec']['install']['spec']['clusterPermissions'].append(
-        {
-            'rules': operator_role['rules'],
-            # TODO: for some reason, the SA is hardcoded here, might be an issue but dismissing for now
-            'serviceAccountName': 'route-monitor-operator',
-        })
-
-csv['spec']['install']['spec']['permissions'] = []
-only_role_yamls = [y for y in yaml.safe_load_all(
-    kustomize_yamls.stdout) if y["kind"] == "Role"]
-for operator_role in only_role_yamls:
-    csv['spec']['install']['spec']['permissions'].append(
-        {
-            'rules': operator_role['rules'],
-            # TODO: for some reason, the SA is hardcoded here, might be an issue but dismissing for now
-            'serviceAccountName': 'route-monitor-operator',
-        })
-
-# this is a hack until https://github.com/kubernetes-sigs/kubebuilder/issues/1894 is merged
-only_service_yamls = [y for y in yaml.safe_load_all(
-    kustomize_yamls.stdout) if y["kind"] == "Service"]
-for operator_service in only_service_yamls:
-    with tempfile.NamedTemporaryFile(mode="w+t",
-                                     prefix='service_', suffix='.yaml',
-                                     dir=version_dir,
-                                     delete=False) as temp_file:
-        yaml.dump(extracted_yaml, temp_file, default_flow_style=False)
-
-# Add our deployment spec for the hive operator:
-kustomize_yamls = subprocess.run(
-    ["kustomize", "build", "config/manager"], stdout=subprocess.PIPE)
-possibly_only_deployment = [y for y in yaml.safe_load_all(
-    kustomize_yamls.stdout) if y["kind"] == "Deployment"]
-amount_of_deployments = len(possibly_only_deployment)
-if amount_of_deployments != 1:
-    print("'kustomize build config/manager' returned an unexpected result, failing early")
-    print("expected one Deployment, got", amount_of_deployments)
-    sys.exit(1)
-
-# There is only one yaml document in the operator deployment
-operator_deployment = possibly_only_deployment[0]
-csv['spec']['install']['spec']['deployments'] = []
-csv['spec']['install']['spec']['deployments'].append({})
-csv['spec']['install']['spec']['deployments'][0]['spec'] = operator_deployment['spec']
-# Update the deployment to use the defined image:
-csv['spec']['install']['spec']['deployments'][0]['spec']['template']['spec']['containers'][0]['image'] = route_monitor_operator_image
 
 # Update the versions to include git hash:
-csv['metadata']['name'] = "route-monitor-operator.v%s" % full_version
-csv['spec']['version'] = full_version
 csv['spec']['replaces'] = "route-monitor-operator.v%s" % prev_version
 
 # Set the CSV createdAt annotation:
 now = datetime.datetime.now()
 csv['metadata']['annotations']['createdAt'] = now.strftime(
     "%Y-%m-%dT%H:%M:%SZ")
+
+# Write all files aside of the csv to disk
+
+
+def is_not_csv_file(filename):
+    """Retun an indication if the file entered is the clusterserviceversion (csv) file """
+    return not filename.endswith('clusterserviceversion.yaml')
+
+
+only_bundle_files = [f for f in os.listdir(BUNDLE_BASE_PATH)
+                     if os.path.isfile(os.path.join(BUNDLE_BASE_PATH, f))
+                     and is_not_csv_file(f)]
+
+for f in only_bundle_files:
+    src_file = os.path.join(BUNDLE_BASE_PATH, f)
+    dst_file = os.path.join(version_dir, f)
+    shutil.copyfile(src_file, dst_file)
 
 # Write the CSV to disk:
 csv_filename = "route-monitor-operator.v%s.clusterserviceversion.yaml" % full_version
