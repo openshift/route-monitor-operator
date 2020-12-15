063a15a589dead260e786bb21ba38f4a2bfbc79e
Manuel Dewald <mdewald@redhat.com>
2020-12-15 12:58:06 +0100

    use local built operator-sdk

--- hack/app_sre_build_deploy.sh
+++ hack/app_sre_build_deploy.sh
@@ -13,17 +13,17 @@ IMG="${BASE_IMG}:latest"
 GIT_HASH=$(git rev-parse --short=7 HEAD)
 
 # build the image
-BUILD_CMD="docker build" IMG="$IMG" make docker-build
+#BUILD_CMD="docker build" IMG="$IMG" make docker-build
 
 # push the image
-skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
-    "docker-daemon:${IMG}" \
-    "docker://${QUAY_IMAGE}:latest"
-
-skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
-    "docker-daemon:${IMG}" \
-    "docker://${QUAY_IMAGE}:${GIT_HASH}"
-
+#skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+#    "docker-daemon:${IMG}" \
+#    "docker://${QUAY_IMAGE}:latest"
+#
+#skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+#    "docker-daemon:${IMG}" \
+#    "docker://${QUAY_IMAGE}:${GIT_HASH}"
+#
 # create and push staging image catalog
 "$CURRENT_DIR"/app_sre_create_image_catalog.sh staging "$QUAY_IMAGE"
 
