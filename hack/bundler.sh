1b0f0a91b717305756b5c73b1515930d794b9eec
NautiluX <mdewald@redhat.com>
2021-01-27 17:56:39 +0100

    fix chmod

--- hack/bundler.sh
+++ hack/bundler.sh
@@ -2,4 +2,5 @@
 set -euxo pipefail
 cd route-monitor-operator
 make packagemanifests
-# chmod 775 -R $BUNDLE_DIR
+NEW_VERSION=$(ls "$BUNDLE_DIR" | sort -t . -k 3 -g | tail -n 1)
+chmod 775 -R $BUNDLE_DIR/$NEW_VERSION
