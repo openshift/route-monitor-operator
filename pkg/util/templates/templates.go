a32316e48d68efba1381a78be336af193b880d5f
Ron Green <11993626+georgettica@users.noreply.github.com>
2021-02-04 12:11:35 +0200

    fix(prometheusrule): make string cleaner

--- pkg/util/templates/templates.go
+++ pkg/util/templates/templates.go
@@ -2,6 +2,7 @@ package templates
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/openshift/route-monitor-operator/pkg/consts/blackbox"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
@@ -113,11 +114,10 @@ func TemplateForPrometheusRuleResource(url, percent string, namespacedName types
 		       targeturl: getmeright
 		*/
 
-		const alertTemplate string = `
-	        	 sum(http_requests_total:burnrate%[3]s{%[1]s}) > (14.40 * (1-%[2]s))
-	        	 and
-	        	 sum(http_requests_total:burnrate%[4]s{%[1]s}) > (14.40 * (1-%[2]s))
-			 `
+		alertTemplate := strings.Join([]string{
+			`sum(http_requests_total:burnrate%[3]s{%[1]s}) > (14.40 * (1-%[2]s))`,
+			`and`,
+			`sum(http_requests_total:burnrate%[4]s{%[1]s}) > (14.40 * (1-%[2]s))`}, "\n")
 		rules = append(rules, monitoringv1.Rule{
 			Alert: "ErrorBudgetBurn",
 			Expr: intstr.FromString(fmt.Sprintf(alertTemplate,
@@ -144,10 +144,10 @@ func TemplateForPrometheusRuleResource(url, percent string, namespacedName types
 	       targeturl: getmeright
 	     record: http_requests_total:burnrate6h
 	*/
-	const recordingRuleTemplate string = `
-        	sum(rate(http_requests_total{%[1]s,code=~"5.."}[%[2]s]))
-        	/
-        	sum(rate(http_requests_total{%[1]s}[%[2]s])) `
+	recordingRuleTemplate := strings.Join([]string{
+		`sum(rate(http_requests_total{%[1]s,code=~"5.."}[%[2]s]))`,
+		`/`,
+		`sum(rate(http_requests_total{%[1]s}[%[2]s]))`}, "\n")
 	for _, timePeriod := range []string{"5m", "30m", "1h", "2h", "6h", "1d", "3d"} {
 		rules = append(rules, monitoringv1.Rule{
 			Expr:   intstr.FromString(fmt.Sprintf(recordingRuleTemplate, routeURLLabel, timePeriod)),
