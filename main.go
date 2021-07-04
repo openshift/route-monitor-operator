feb16b7035e76653abb772617cc17f274465e50a
Ron Green <11993626+georgettica@users.noreply.github.com>
2021-07-04 13:48:28 +0300

    fix(manager): use --config flag (part 2)
    in the prev commit the --config was used, now it actually loads it
    
    in the new vesions of ocm-container, there is a file that describes some
    config options, this is required as some new feats are introduced there
    
    to allow using it we needed to add a --config flag
    
    taken from https://github.com/operator-framework/operator-sdk/commit/c48d4ddad1b90dc17f0736bb1372371a2704f975#diff-53c42b6dedd270af61f74a2cc8f5649bcfd53a33d67e2eb6419dc3a8b50edc9bR107-R112

--- main.go
+++ main.go
@@ -60,11 +60,19 @@ func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
+	var managerConfigPath string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
+	flag.StringVar(&managerConfigPath,
+		"config",
+		"",
+		"The controller will load its initial configuration from this file. "+
+			"Omit this flag to use the default configuration values. "+
+			"Command-line flags override configuration from this file.",
+	)
 
 	var blackboxExporterImage string
 	var blackboxExporterNamespace string
@@ -78,14 +86,26 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	options := ctrl.Options{
 		Scheme:                 scheme,
 		MetricsBindAddress:     metricsAddr,
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "2793210b.openshift.io",
-	})
+	}
+
+	var err error
+
+	if managerConfigPath != "" {
+		cfgLoader := ctrl.ConfigFile().AtPath(managerConfigPath)
+		if options, err = options.AndFrom(cfgLoader); err != nil {
+			setupLog.Error(err, "Unable to load the manager config file")
+			os.Exit(1)
+		}
+	}
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), options)
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
