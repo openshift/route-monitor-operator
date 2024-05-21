6d3100ee5dccaf511a1526a058851abb436f2b8e
Yiqin Zhang <yiqzhang@redhat.com>
2024-05-21 14:10:13 -0400

    recover prometheus-operator in go.mod and go.sum

--- go.mod
+++ go.mod
@@ -3,28 +3,30 @@ module github.com/openshift/route-monitor-operator
 go 1.21
 
 require (
-	github.com/go-logr/logr v1.2.3
+	github.com/go-logr/logr v1.4.1
 	github.com/google/gofuzz v1.2.0
 	github.com/hashicorp/go-version v1.6.0
 	github.com/onsi/ginkgo v1.16.5
-	github.com/onsi/gomega v1.27.4
+	github.com/onsi/ginkgo/v2 v2.9.5
+	github.com/onsi/gomega v1.27.7
 	github.com/openshift/api v0.0.0-20230213134911-7ba313770556
 	github.com/openshift/hypershift v0.0.0-20230320142408-f88fd0deab44
-	github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring v0.63.0
-	github.com/prometheus/common v0.42.0
+	github.com/openshift/osde2e-common v0.0.0-20240404214304-9eefff9c607b
+	github.com/prometheus-operator/prometheus-operator/pkg/client v0.64.0
+	github.com/prometheus/common v0.43.0
 	github.com/rhobs/obo-prometheus-operator/pkg/apis/monitoring v0.60.0-rhobs1
 	go.uber.org/mock v0.4.0
 	gopkg.in/inf.v0 v0.9.1
-	k8s.io/api v0.26.3
-	k8s.io/apiextensions-apiserver v0.26.3
-	k8s.io/apimachinery v0.26.3
+	k8s.io/api v0.28.2
+	k8s.io/apiextensions-apiserver v0.27.2
+	k8s.io/apimachinery v0.28.2
 	k8s.io/client-go v12.0.0+incompatible
-	k8s.io/kube-openapi v0.0.0-20230308215209-15aac26d736a
-	sigs.k8s.io/controller-runtime v0.14.4
+	k8s.io/kube-openapi v0.0.0-20230717233707-2695361300d9
+	sigs.k8s.io/controller-runtime v0.15.1
 )
 
 require (
-	cloud.google.com/go/compute v1.14.0 // indirect
+	cloud.google.com/go/compute v1.20.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/aws/aws-sdk-go v1.44.225 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -34,64 +36,68 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.10.2 // indirect
 	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
+	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/go-logr/zapr v1.2.3 // indirect
+	github.com/go-logr/zapr v1.2.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/gobuffalo/flect v1.0.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/gnostic v0.6.9 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/gnostic-models v0.6.8 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/pprof v0.0.0-20210720184732-4bb14d4b1be1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/imdario/mergo v0.3.14 // indirect
+	github.com/imdario/mergo v0.3.15 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/moby/spdystream v0.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus/client_golang v1.14.0 // indirect
-	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/client_golang v1.15.1 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
 	github.com/prometheus/procfs v0.9.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.8.2 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/oauth2 v0.15.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/term v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.19.0 // indirect
-	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/component-base v0.26.3 // indirect
-	k8s.io/klog/v2 v2.90.1 // indirect
-	k8s.io/utils v0.0.0-20230313181309-38a27ef9d749 // indirect
+	k8s.io/component-base v0.27.2 // indirect
+	k8s.io/klog/v2 v2.120.1 // indirect
+	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2 // indirect
 	sigs.k8s.io/cluster-api v1.3.5 // indirect
 	sigs.k8s.io/cluster-api-provider-aws/v2 v2.0.2 // indirect
 	sigs.k8s.io/cluster-api-provider-ibmcloud v0.4.0 // indirect
+	sigs.k8s.io/e2e-framework v0.3.0 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
 
 replace (
-	k8s.io/apimachinery => k8s.io/apimachinery v0.25.2
-	k8s.io/client-go => k8s.io/client-go v0.25.2
+	k8s.io/apimachinery => k8s.io/apimachinery v0.28.2
+	k8s.io/client-go => k8s.io/client-go v0.28.2
 	sigs.k8s.io/cluster-api-provider-kubevirt => github.com/openshift/cluster-api-provider-kubevirt v0.0.0-20211223062810-ef64d5ff1cde
-	sigs.k8s.io/controller-runtime => sigs.k8s.io/controller-runtime v0.12.3
 )
---
+++ osde2e/route_monitor_operator_tests.go
@@ -0,0 +1,243 @@
+// DO NOT REMOVE TAGS BELOW. IF ANY NEW TEST FILES ARE CREATED UNDER /osde2e, PLEASE ADD THESE TAGS TO THEM IN ORDER TO BE EXCLUDED FROM UNIT TESTS. //go:build osde2e
+//go:build osde2e
+// +build osde2e
+
+package osde2etests
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	routev1 "github.com/openshift/api/route/v1"
+	"github.com/openshift/osde2e-common/pkg/clients/openshift"
+	. "github.com/openshift/osde2e-common/pkg/gomega/assertions"
+	. "github.com/openshift/osde2e-common/pkg/gomega/matchers"
+	routemonitorv1alpha1 "github.com/openshift/route-monitor-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	kubev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/dynamic"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var _ = Describe("Route Monitor Operator", Ordered, func() {
+	var (
+		k8s                   *openshift.Client
+		serviceMonitorsClient dynamic.ResourceInterface
+		prometheusRulesClient dynamic.ResourceInterface
+	)
+	const (
+		namespace        = "openshift-route-monitor-operator"
+		deploymentName   = "route-monitor-operator-controller-manager"
+		operatorName     = "route-monitor-operator"
+		consoleName      = "console"
+		pollingDuration  = 10 * time.Minute
+		routeMonitorName = "routemonitor-e2e-test"
+	)
+	BeforeAll(func(ctx context.Context) {
+		log.SetLogger(GinkgoLogr)
+		var err error
+		k8s, err = openshift.New(GinkgoLogr)
+		Expect(err).ShouldNot(HaveOccurred(), "unable to setup k8s client")
+		dynamicClient, err := dynamic.NewForConfig(k8s.GetConfig())
+		Expect(err).ShouldNot(HaveOccurred(), "failed creating the dynamic client: %w", err)
+		Expect(routemonitorv1alpha1.AddToScheme(k8s.GetScheme())).Should(Succeed(), "unable to register RouteMonitor scheme")
+		serviceMonitorsClient = dynamicClient.Resource(schema.GroupVersionResource{
+			Group:    "monitoring.openshift.io",
+			Version:  "v1alpha1",
+			Resource: "routemonitors",
+		}).Namespace(namespace)
+		prometheusRulesClient = dynamicClient.Resource(schema.GroupVersionResource{
+			Group:    "monitoring.coreos.com",
+			Version:  "v1",
+			Resource: "prometheusrules",
+		}).Namespace(namespace)
+	})
+
+	It("is installed", func(ctx context.Context) {
+		By("checking the namespace exists")
+		err := k8s.Get(ctx, namespace, "", &corev1.Namespace{})
+		Expect(err).ShouldNot(HaveOccurred(), "namespace %s not found", namespace)
+
+		By("checking the deployment exists and is available")
+		EventuallyDeployment(ctx, k8s, deploymentName, namespace).Should(BeAvailable())
+	})
+
+	It("can be upgraded", func(ctx context.Context) {
+		By("forcing operator upgrade")
+		err := k8s.UpgradeOperator(ctx, operatorName, namespace)
+		Expect(err).ShouldNot(HaveOccurred(), "operator upgrade failed")
+	})
+
+	It("has all of the required resources", func(ctx context.Context) {
+		_, err := serviceMonitorsClient.Get(ctx, consoleName, metav1.GetOptions{})
+		Expect(err).ShouldNot(HaveOccurred(), "Unable to get console serviceMonitor")
+
+		_, err = prometheusRulesClient.Get(ctx, consoleName, metav1.GetOptions{})
+		Expect(err).ShouldNot(HaveOccurred(), "Unable to get console prometheusRule")
+	})
+
+	It("required dependent resources are created", func(ctx context.Context) {
+		By("Creating a pod, service and route to monitor with a ServiceMonitor and PrometheusRule")
+		var (
+			pod      *corev1.Pod
+			svc      *corev1.Service
+			appRoute *routev1.Route
+			rmo      *routemonitorv1alpha1.RouteMonitor
+		)
+		By("Creating the test pod")
+		pod = &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      routeMonitorName,
+				Namespace: namespace,
+			},
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{
+						Name:  "test",
+						Image: "quay.io/jitesoft/nginx:mainline",
+					},
+				},
+			},
+		}
+		err := k8s.Create(ctx, pod)
+		Expect(err).ShouldNot(HaveOccurred(), "Unable to create a pod")
+
+		By("Checking the pod state")
+		var phase kubev1.PodPhase
+		err = wait.PollUntilContextTimeout(ctx, 15*time.Second, 2*time.Minute, false, func(ctx context.Context) (bool, error) {
+			err := k8s.Get(ctx, pod.Name, pod.Namespace, pod)
+			Expect(err).ShouldNot(HaveOccurred(), "Unable to get the testing pod")
+			if pod != nil {
+				phase = pod.Status.Phase
+				if phase == kubev1.PodRunning || phase == kubev1.PodFailed {
+					return true, nil
+				}
+			}
+			GinkgoLogr.Info(fmt.Sprintf("Waiting for Pod '%s/%s' to be %s, currently %s...", pod.Namespace, pod.Name, kubev1.PodRunning, phase))
+			return phase == kubev1.PodRunning, nil
+		})
+		Expect(err).ShouldNot(HaveOccurred(), fmt.Sprintf("Pod %s in ns %s is not running, current pod state is %v", routeMonitorName, namespace, phase))
+
+		By("Creating the service")
+		svc = &corev1.Service{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      routeMonitorName,
+				Namespace: namespace,
+				Labels:    map[string]string{routeMonitorName: routeMonitorName},
+			},
+			Spec: corev1.ServiceSpec{
+				Ports: []corev1.ServicePort{
+					{
+						Port:       8080,
+						Protocol:   corev1.ProtocolTCP,
+						TargetPort: intstr.FromInt(80),
+						Name:       "web",
+					},
+				},
+				Selector: map[string]string{routeMonitorName: routeMonitorName},
+			},
+		}
+
+		err = k8s.Create(ctx, svc)
+		Expect(err).ShouldNot(HaveOccurred(), "Unable to create service %s/%s", svc.Namespace, svc.Name)
+
+		By("Creating the route")
+		appRoute = &routev1.Route{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: namespace,
+				Name:      routeMonitorName,
+			},
+			Spec: routev1.RouteSpec{
+				To: routev1.RouteTargetReference{
+					Name: routeMonitorName,
+				},
+				TLS: &routev1.TLSConfig{Termination: "edge"},
+			},
+			Status: routev1.RouteStatus{},
+		}
+
+		err = k8s.Create(ctx, appRoute)
+		Expect(err).ShouldNot(HaveOccurred(), "Unable to create route %s/%s", appRoute.Namespace, appRoute.Name)
+
+		By("Creating a sample RouteMonitor to monitor the service")
+		rmo = &routemonitorv1alpha1.RouteMonitor{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: namespace,
+				Name:      routeMonitorName,
+			},
+			Spec: routemonitorv1alpha1.RouteMonitorSpec{
+				Slo: routemonitorv1alpha1.SloSpec{
+					TargetAvailabilityPercent: "99.95",
+				},
+				Route: routemonitorv1alpha1.RouteMonitorRouteSpec{
+					Namespace: namespace,
+					Name:      routeMonitorName,
+				},
+			},
+		}
+
+		err = k8s.Create(ctx, rmo)
+		Expect(err).ShouldNot(HaveOccurred(), "Unable to create RouteMonitor %s/%s", rmo.Namespace, rmo.Name)
+
+		By("Checking the dependent resources are created")
+		err = wait.PollUntilContextTimeout(ctx, 15*time.Second, pollingDuration, false, func(ctx context.Context) (bool, error) {
+			_, err = serviceMonitorsClient.Get(ctx, routeMonitorName, metav1.GetOptions{})
+			if k8serrors.IsNotFound(err) {
+				return false, err // Continue polling if resource is not found
+			}
+			if err != nil {
+				return false, err // Return error to stop polling if other errors occur
+			}
+
+			_, err = prometheusRulesClient.Get(ctx, routeMonitorName, metav1.GetOptions{})
+			if k8serrors.IsNotFound(err) {
+				return false, err
+			}
+			if err != nil {
+				return false, err
+			}
+			return true, nil
+		})
+		Expect(err).ShouldNot(HaveOccurred(), "Dependant resources weren't created via RouteMonitor")
+
+		By("Deleting the sample RouteMonitor")
+		err = k8s.Delete(ctx, rmo)
+		Expect(err).ShouldNot(HaveOccurred(), "Failed to delete RouteMonitor")
+
+		// wait for the routemonitor to delete before returning.
+		err = wait.PollUntilContextTimeout(ctx, 2*time.Second, 1*time.Minute, false, func(ctx context.Context) (bool, error) {
+			err = k8s.Get(ctx, rmo.Name, rmo.Namespace, rmo)
+			if k8serrors.IsNotFound(err) {
+				return true, nil // RouteMonitor is deleted
+			}
+			if err != nil {
+				return false, err // Return error to stop retrying
+			}
+			return false, nil // RouteMonitor still exists, continue polling
+		})
+		Expect(err).ShouldNot(HaveOccurred(), "Couldn't delete application route monitor")
+
+		_, err = serviceMonitorsClient.Get(ctx, routeMonitorName, metav1.GetOptions{})
+		Expect(k8serrors.IsNotFound(err)).To(BeTrue(), "Sample serviceMonitor still exists, deletion of RouteMonitor didn't clean it up")
+		_, err = prometheusRulesClient.Get(ctx, routeMonitorName, metav1.GetOptions{})
+		Expect(k8serrors.IsNotFound(err)).To(BeTrue(), "Sample prometheusRule still exists, deletion of RouteMonitor didn't clean it up")
+
+		DeferCleanup(func(ctx context.Context) {
+			By("Cleaning up setup")
+			By("Deleting test pod " + routeMonitorName)
+			Expect(k8s.Delete(ctx, pod)).Should(Succeed(), "Failed to delete pod")
+			By("Deleting test service " + routeMonitorName)
+			Expect(k8s.Delete(ctx, svc)).Should(Succeed(), "Failed to delete service")
+			By("Deleting test route " + routeMonitorName)
+			Expect(k8s.Delete(ctx, appRoute)).Should(Succeed(), "Failed to delete route")
+		})
+	})
+})
